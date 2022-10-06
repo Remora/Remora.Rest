d05bf4ed89f7ffd2377f0236a379cba8fab8859d
uwx <cadrekucra@gmx.com>
2022-10-06 20:11:40 -0300

    Call Span<byte> overloads in byte[] overloads of IsPNG/IsGIF/IsJPG
    (cherry picked from commit d0bd1dc912918173cc3b434150ae9b90bfb92e6a)

--- Remora.Rest/Extensions/TypeDetectionExtensions.cs
+++ Remora.Rest/Extensions/TypeDetectionExtensions.cs
@@ -34,22 +34,7 @@ public static class TypeDetectionExtensions
     /// </summary>
     /// <param name="array">The array.</param>
     /// <returns>true if the array contains PNG data; otherwise, false.</returns>
-    public static bool IsPNG(this byte[] array)
-    {
-        if (array.Length < 8)
-        {
-            return false;
-        }
-
-        return array[0] == 0x89 &&
-               array[1] == 0x50 &&
-               array[2] == 0x4E &&
-               array[3] == 0x47 &&
-               array[4] == 0x0D &&
-               array[5] == 0x0A &&
-               array[6] == 0x1A &&
-               array[7] == 0x0A;
-    }
+    public static bool IsPNG(this byte[] array) => IsPNG(array.AsSpan());
 
     /// <summary>
     /// Determines whether the span is a view over PNG data.
@@ -78,15 +63,7 @@ public static class TypeDetectionExtensions
     /// </summary>
     /// <param name="array">The array.</param>
     /// <returns>true if the array contains GIF data; otherwise, false.</returns>
-    public static bool IsGIF(this byte[] array)
-    {
-        if (array.Length < 3)
-        {
-            return false;
-        }
-
-        return array[0] == 0x47 && array[1] == 0x49 && array[2] == 0x46;
-    }
+    public static bool IsGIF(this byte[] array) => IsGIF(array.AsSpan());
 
     /// <summary>
     /// Determines whether the span is a view over GIF data.
@@ -108,15 +85,7 @@ public static class TypeDetectionExtensions
     /// </summary>
     /// <param name="array">The array.</param>
     /// <returns>true if the array contains JPG data; otherwise, false.</returns>
-    public static bool IsJPG(this byte[] array)
-    {
-        if (array.Length < 3)
-        {
-            return false;
-        }
-
-        return array[0] == 0xFF && array[1] == 0xD8 && array[2] == 0xFF;
-    }
+    public static bool IsJPG(this byte[] array) => IsJPG(array.AsSpan());
 
     /// <summary>
     /// Determines whether the span is a view over JPG data.
